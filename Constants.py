20cb5aa9f00fddb970d2585ff8da057a71e646b3
Tony Ehlert <aehlert@dmacc.edu>
2023-02-01 09:43:15 -0600

    Added Constants.py to project

---
+++ Constants.py
@@ -0,0 +1,9 @@
+"""
+Program: Constants.py
+Author: Tony Ehlert
+Last date modified: 01/25/2023
+
+The purpose of this file is to list and declare constants to be used
+"""
+# Constant variable representing total number of scores
+NUM_OF_SCORES = 3
